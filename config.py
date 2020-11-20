93464a173171916a2f47858412b1853c75f83d72
Koyo Nakamura <koyo@uw.edu>
2020-11-20 14:34:51 -0800

    remove config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# Template file - DO NOT PUSH TO REPOSITORY
-# To stop tracking the file,
-# 1. use .gitignore and ignore config.py
-# 2. open git bash in repository and type "git rm --cached config.py"
-
-consumer_key = "hello"
-consumer_secret = ""
-access_token = ""
-access_token_secret = ""
